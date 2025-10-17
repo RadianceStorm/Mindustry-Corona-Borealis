e8bc3ec079036057a5ce17c3af4a597fc5df36c5
Radiance <75605195+RadianceStorm@users.noreply.github.com>
2025-10-16 22:43:03 -0700

    Created .java file for mod
    yeah

--- src/example/CoronaBorealisMod.java
+++ src/example/CoronaBorealisMod.java
@@ -9,19 +9,19 @@ import mindustry.gen.*;
 import mindustry.mod.*;
 import mindustry.ui.dialogs.*;
 
-public class ExampleJavaMod extends Mod{
+public class CoronaBorealisMod extends Mod{
 
-    public ExampleJavaMod(){
-        Log.info("Loaded ExampleJavaMod constructor.");
+    public CoronaBorealisMod(){
+        Log.info("Loaded CoronaBorealisMod constructor.");
 
         //listen for game load event
         Events.on(ClientLoadEvent.class, e -> {
             //show dialog upon startup
             Time.runTask(10f, () -> {
-                BaseDialog dialog = new BaseDialog("frog");
+                BaseDialog dialog = new BaseDialog("Corona Borealis Mod import successful!");
                 dialog.cont.add("behold").row();
-                //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
-                dialog.cont.image(Core.atlas.find("example-java-mod-frog")).pad(20f).row();
+                //mod sprites are prefixed with the mod name (this mod is called 'corona-borealis' in its config)
+                dialog.cont.image(Core.atlas.find("corona-borealis-frog")).pad(20f).row();
                 dialog.cont.button("I see", dialog::hide).size(100f, 50f);
                 dialog.show();
             });
@@ -30,7 +30,7 @@ public class ExampleJavaMod extends Mod{
 
     @Override
     public void loadContent(){
-        Log.info("Loading some example content.");
+        Log.info("Loading some Corona Borealis content.");
     }
 
 }
